5f74c7d197212e5d106b43fd894d3b2bfe90596f
Alwin den Herder <42200913+Alwindh@users.noreply.github.com>
2022-05-30 23:44:50 +0200

    initial update

--- src/components/flippingItem.comp.js
+++ src/components/flippingItem.comp.js
@@ -22,7 +22,26 @@ export default function FlippingItem(props) {
 		<Accordion TransitionProps={{ unmountOnExit: true }} key={props.inputItem.itemId + props.inputItem.itemName}>
 			<Paper elevation={8}>
 				<AccordionSummary expandIcon={<ExpandMoreIcon />} aria-controls="panel1a-content" id="panel1a-header">
-					<Typography sx={{ width: "33%", flexShrink: 0 }}>{props.inputItem.itemName}</Typography>
+					<img
+						style={{ marginTop: "-4px", marginBottom: "-4px", marginRight: "1em" }}
+						height="32px"
+						alt=""
+						src={
+							"https://universalis-ffxiv.github.io/universalis-assets/icon2x/" +
+							props.inputItem.itemId +
+							".png"
+						}
+					/>
+					<Typography sx={{ width: "33%", flexShrink: 0 }}>
+						{props.inputItem.itemName.slice(props.inputItem.itemName.length - 2) === "HQ"
+							? props.inputItem.itemName.slice(0, props.inputItem.itemName.length - 2)
+							: props.inputItem.itemName}
+						{props.inputItem.itemName.slice(props.inputItem.itemName.length - 2) === "HQ" ? (
+							<img height="16px" style={{ marginLeft: "0.5em" }} alt="hq" src="/hq.png" />
+						) : (
+							""
+						)}
+					</Typography>
 					<Typography sx={{ width: "33%", color: "text.secondary" }}>
 						{props.inputItem.lowestTwinPrice + " Gil"}
 					</Typography>
@@ -44,21 +63,27 @@ export default function FlippingItem(props) {
 					{props.inputItem.viableListings.slice(0, 10).map((listing) => {
 						return (
 							<Grid container spacing={2} key={listing.listingId}>
+								<Grid item xs={1}></Grid>
 								<Grid item xs={1}>
-									{listing.pricePerUnit}
+									{listing.pricePerUnit + " gil"}
 								</Grid>
 								<Grid item xs={1}>
 									{listing.quantity}
 								</Grid>
 								<Grid item xs={1}>
-									{listing.hq ? "HQ" : ""}
+									{listing.hq ? (
+										<img height="16px" style={{ marginLeft: "0.5em" }} alt="hq" src="/hq.png" />
+									) : (
+										""
+									)}
 								</Grid>
-								<Grid item xs={4}>
+								<Grid item xs={3}>
 									{listing.worldName}
 								</Grid>
-								<Grid item xs={5}>
+								<Grid item xs={3}>
 									{listing.timeString}
 								</Grid>
+								<Grid item xs={1}></Grid>
 							</Grid>
 						);
 					})}
